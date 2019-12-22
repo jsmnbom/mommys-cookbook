6693ac15abd5fb52b4507289e2f9e4efcd3684ee
Jasmin Bom <jsmnbom@gmail.com>
2019-12-22 12:18:12 +0100

    Remove unused slide transition

--- src/App.vue
+++ src/App.vue
@@ -41,10 +41,10 @@ export default Vue.extend({
   },
   computed: {
     ...mapState("account", ["loggedIn"]),
-    loading() {
+    loading(): boolean {
       return !this.$store.state.account.loaded;
     },
-    routerKey() {
+    routerKey(): string {
       let key = "";
       if (this.$route.name) {
         key += this.$route.name;
@@ -64,12 +64,6 @@ export default Vue.extend({
     this.$router.beforeEach((to, from, next) => {
       let transitionName = to.meta.transitionName || from.meta.transitionName;
 
-      if (transitionName === "slide") {
-        const toDepth = to.path.split("/").length;
-        const fromDepth = from.path.split("/").length;
-        transitionName = toDepth < fromDepth ? "slide-right" : "slide-left";
-      }
-      // @ts-ignore
       this.transitionName = transitionName || DEFAULT_TRANSITION;
 
       next();
@@ -119,27 +113,6 @@ export default Vue.extend({
 .fade-enter,
 .fade-leave-active {
   opacity: 0;
-}
-.slide-left-enter-active,
-.slide-left-leave-active,
-.slide-right-enter-active,
-.slide-right-leave-active {
-  transition-duration: 0.5s;
-  transition-property: height, opacity, transform;
-  transition-timing-function: cubic-bezier(0.55, 0, 0.1, 1);
-  overflow: hidden;
-}
-
-.slide-left-enter,
-.slide-right-leave-active {
-  opacity: 0;
-  transform: translate(2em, 0);
-}
-
-.slide-left-leave-active,
-.slide-right-enter {
-  opacity: 0;
-  transform: translate(-2em, 0);
 }
 
 body {
